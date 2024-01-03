7fcb1f0227b180ee02ca92eb456259e9a9811e61
Rafael-ASS <rarafaelalgusto@gmail.com>
2024-01-03 16:49:49 -0300

    Removendo a tabela permição

--- src/main/java/br/edu/ifrn/GeographyQuiz/domain/quiz/Quiz.java
+++ src/main/java/br/edu/ifrn/GeographyQuiz/domain/quiz/Quiz.java
@@ -6,6 +6,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
@@ -15,11 +17,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.edu.ifrn.GeographyQuiz.domain.pergunta.*;
+import br.edu.ifrn.GeographyQuiz.domain.usuario.Usuario;
 
 @Entity(name = "quiz")    //JPQL
 @Table(name = "quiz")     //SQL
@@ -32,4 +36,30 @@ public class Quiz {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "O Nome do quiz obrigatório")
+    @Column(name = "nome")
+    private String nome;
+
+    @NotBlank(message = "A Descrição do quiz obrigatório")
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "nota")
+    private Integer nota;
+
+    @Column(name = "pontuacao")
+    private Integer pontuacao;
+
+    @Column(name = "data_hora")
+    private LocalDate data_hora;
+
+    @ManyToOne
+    @JoinColumn(name = "jogador_atual_id")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "pergunta_atual_id")
+    private Pergunta pergunta;
+    
 }
--- src/main/java/br/edu/ifrn/GeographyQuiz/domain/usuario/Usuario.java
+++ src/main/java/br/edu/ifrn/GeographyQuiz/domain/usuario/Usuario.java
@@ -1,24 +1,19 @@
 package br.edu.ifrn.GeographyQuiz.domain.usuario;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Collections;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import br.edu.ifrn.GeographyQuiz.domain.permissao.Permissao;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -36,24 +31,24 @@ public class Usuario implements UserDetails{
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private String usuario;
-  private String senha;
-  private String email;
 
-  @ManyToMany(fetch = FetchType.EAGER)
-  @JoinTable(name = "usuario_permissao", 
-  joinColumns = @JoinColumn(name = "usuario_id"), 
-  inverseJoinColumns = @JoinColumn(name = "permissao_id"))
-  private Set<Permissao> permissoes;
+  @NotBlank(message = "O nome de usuario é obrigatório")
+  @Column(name = "usuario")
+  private String usuario;
+
+  @NotBlank(message = "A é obrigatório")
+  @Column(name = "senha")
+  private String senha;
+
+  @NotBlank(message = "O email é obrigatório")
+  @Column(name = "email")
+  private String email;
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    Collection<? extends GrantedAuthority> authorities = this.permissoes.stream().map(
-      permissao -> new SimpleGrantedAuthority(permissao.getNome())).
-      collect(Collectors.toList());
-      System.out.println(this.email + "  " + " " + authorities.toString());
-      return authorities;
+    return Collections.singletonList(new SimpleGrantedAuthority(email));
   }
+
   @Override
   public String getPassword() {
     return this.senha;
